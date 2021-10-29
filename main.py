273dc7eb936b20e5ef01eb35d02d55bccec2fe62
MelchiGit <melchiorphilips@gmail.com>
2021-10-29 21:17:16 +0200

    Updated code to working version.

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-pip install lyricsgenius
-
 import re
+import os
+import lyricsgenius
 
-# Open a file and store contents
-textFile = open('G:\Dropbox\Dropbox\Personal\Projects\RapGot\Lyrics.txt', encoding="utf8", mode='r')
-fileContent = textFile.read()
-textFile.close()
+geniusAccessToken = ''
+lyricsPath = ''
+artistName = "Eminem"
 
-# pattern  = re.compile(r'[\[\(]p\. \d+[\]\)]') #Page number text
-# pattern = re.compile(r'\n\n\n+') #Consecutive whitespaces
-# pattern = re.compile(r'\W-\n\n') #Non-oordafbrekingen
-# pattern = re.compile(r'-\n\n') #Woordafbrekingen
-# pattern = re.compile(r'([^\.!?:;])(\n\n)') #paragrafen die zijn afgebroken middenin een zin - eindigt niet met .!?:;
-# pattern = re.compile(r'\n\n\s+') #Consecutive whitespaces
+# Set up Genius.com API Client
+genius = lyricsgenius.Genius(geniusAccessToken)
+genius.verbose = False # Turn off status messages
+genius.remove_section_headers = True # Remove section headers (e.g. [Chorus]) from lyrics when searching
+genius.skip_non_songs = True # Exclude hits thought to be non-songs (e.g. track lists)
+genius.excluded_terms = ["(Remix)", "(Live)", "Freestyle", "(Skit)", "Intro"] # Exclude songs with these words in their title
 
-appendedText = re.sub(r'\[.*?\]', '', fileContent) # Remove text in square brackets
-appendedText = re.sub(r'\'', '', fileContent) # remove aposotrophes (')
+# get most popular song titles from Eminem
+rapper = genius.search_artist(artistName, max_songs=50, sort="popularity")
+for song in rapper.songs:
+    print(song.title)
 
-print(appendedText)
+    fileContent = song.lyrics
 
-# Write to text file
-# Open a file and store contents
-textFile = open('G:\Dropbox\Dropbox\Personal\Projects\RapGot\Lyrics.txt', encoding="utf8", mode='w')
-textFile.truncate(0)
-textFile.write(appendedText)
+    appendedText = re.sub(r'\[.*?\]', '', fileContent)  # Remove text in square brackets
+    appendedText = re.sub(r'[\'\"]', '', appendedText)  # remove aposotrophes (' & ")
 
-# Close the file
-textFile.close()
+    songTitle = song.title
+    appendedTitle = re.sub(r'[\'\"\\/.,<>]', '', songTitle)  # remove aposotrophes (' & ")
+
+    # Write to text file
+    # Open a file and store contents
+    if not os.path.exists(lyricsPath + song.artist):
+        os.mkdir(lyricsPath + song.artist)
+    textFile = open(lyricsPath + song.artist + '\\' + appendedTitle + '.txt', encoding="utf8", mode='w+')
+    textFile.truncate(0)
+    textFile.write(appendedText)
+
+    # Close the file
+    textFile.close()
